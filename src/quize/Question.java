59b95ca9cec9edd8e378fb6ddf6ba41d0fe531af
Ganesh Sonawane <ganesh103999@gmail.com>
2025-02-01 20:53:07 +0530

    variable add in question

--- src/quize/Question.java
+++ src/quize/Question.java
@@ -3,6 +3,7 @@ package quize;
 public class Question {
 
 private  String name;
+private  String id;
     private String questionText;
     private String[] options;
     private int correctAnswer; // Will use Index correct answer
